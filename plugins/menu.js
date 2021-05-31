fca81c302ddd4f8567ad3bf7ac3b83daeb7ad207
Genau Mein Ding <84249632+diegottinphia@users.noreply.github.com>
2021-05-31 15:58:12 +0200

    Add files via upload

--- plugins/menu.js
+++ plugins/menu.js
@@ -102,10 +102,10 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 *║┊:* ◦ 📲Sistema operativo : *${conn.user.phone.device_manufacturer}*
 *║┊:* ◦ 🪀version de *WhatsApp* : ${conn.user.phone.wa_version}
 *║┊:* 👑𝐌𝐲 𝐜𝐚𝐧𝐚𝐥 𝐝𝐞 𝐲𝐨𝐮𝐭𝐮𝐛𝐞
-*║┊:* https://www.youtube.com/watch?v=chMc57gjmkI&t=88s
+*║┊:* https://www.youtube.com/channel/UC6TVJZMr2azDk482LdKXwNA
 *║┊:* 𝑇ℎ𝑎𝑛𝑘𝑠 𝑡𝑜 
-*║┊:* @Samu330
-*║┊:* ❤Sam y Perry👑
+*║┊:* @diegottinphia
+*║┊:* 🌴༟𝕻𝖍𝖎ͣ𝖆ͫ༞🌴
 *╰═┅ৡৢ͜͡✦═══╡𝗦𝗮𝗺𝘂𝟯𝟯𝟬╞═══┅ৡৢ͜͡✦═╯*
 %readmore
 *╭═┅〘🛑 𝗢𝗯𝗲𝗱𝗲𝗰𝗲 𝗹𝗮𝘀 𝗿𝗲𝗴𝗹𝗮𝘀 🛑〙*
